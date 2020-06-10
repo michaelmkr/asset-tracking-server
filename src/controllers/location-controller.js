9a0dee8de8c043e5a81eef8ce9264155e2b15674
MichaelMkr <michael.macher@humanizing.com>
2020-06-10 10:34:55 +0200

    added the details from the last meeting as comments

--- src/controllers/location-controller.js
+++ src/controllers/location-controller.js
@@ -16,6 +16,31 @@ const hardcodedLocations = [
   },
 ];
 
+// Infos from BC
+// lat
+// long
+// timestamp
+// isSet
+// isActive
+
+/*
+{
+  "id": 5213848,
+  "location": {
+    "lat": 1,
+    "lng": 2,
+    "timestamp": 1589444836
+},
+  "allowedLocation": {
+    "lat": 1,
+    "lng": 2,
+    "radius": 5
+},
+  "customer": "Casino Baden",
+  "valid-until": "15.05.2021"
+}
+*/
+
 async function getAll() {
   // const result = await LocationController.assist(data);
   return hardcodedLocations;
