156b2381a4f5c4cdd77567604096867e64c1398d
MichaelMkr <michael.macher@humanizing.com>
2020-07-02 20:58:28 +0200

    added location history and location updating

--- src/controllers/location-controller.js
+++ src/controllers/location-controller.js
@@ -6,7 +6,7 @@ const details = [
     location: {
       lat: 48.2138508,
       lng: 15.6296502,
-      timestamp: 1589444836,
+      timestamp: 1593712794109,
     },
     allowedLocation: {
       lat: 48.2138508,
@@ -17,13 +17,14 @@ const details = [
     customer: 'FH St. Pölten GmbH',
     validUntil: '15.05.2021',
     active: true,
+    history: [],
   },
   {
     id: 1,
     location: {
       lat: 50.9412818,
       lng: 6.9560927,
-      timestamp: 1589444836,
+      timestamp: 1593712794109,
     },
     allowedLocation: {
       lat: 50.9412818,
@@ -34,13 +35,14 @@ const details = [
     customer: 'Stadt Köln',
     validUntil: '15.05.2021',
     active: true,
+    history: [],
   },
   {
     id: 2,
     location: {
       lat: 43.7669973,
       lng: 11.2459052,
-      timestamp: 1589444836,
+      timestamp: 1593712794109,
     },
     allowedLocation: {
       lat: 43.7669973,
@@ -51,13 +53,14 @@ const details = [
     customer: 'Stadt Florenz in Italien',
     validUntil: '15.05.2021',
     active: false,
+    history: [],
   },
   {
     id: 3,
     location: {
       lat: 48.2819,
       lng: 15.7118,
-      timestamp: 1589444836,
+      timestamp: 1593712794109,
     },
     allowedLocation: {
       lat: 48.2819,
@@ -68,8 +71,29 @@ const details = [
     customer: 'Christoph Braun',
     validUntil: '15.05.2021',
     active: true,
+    history: [],
   },
 ];
+
+const latCell = {
+  timestamp: 0,
+  value: 0,
+};
+
+const lonCell = {
+  timestamp: 0,
+  value: 0,
+};
+
+const latGps = {
+  timestamp: 0,
+  value: 0,
+};
+
+const lonGps = {
+  timestamp: 0,
+  value: 0,
+};
 
 /*
 Infos from BC
@@ -87,7 +111,6 @@ Advantage of the Blockchain -> a good showcase -> Drone showcase
 async function getAll() {
   const locations = [];
   details.forEach((element) => {
-
     let isInZone = false;
     const start = { latitude: element.location.lat, longitude: element.location.lng };
     const end = { latitude: element.allowedLocation.lat, longitude: element.allowedLocation.lng };
@@ -123,43 +146,42 @@ async function updateDetails(payload) {
   return '422 invalid id';
 }
 
-const latCell = {
-  timestamp: 0,
-  value: 0,
-};
+function checkForLocationUpdate(id, lat, lng, timestamp) {
+  console.log('updating data:\n', lat, lng, timestamp);
+  const start = { latitude: details[id].location.lat, longitude: details[id].location.lng };
+  const end = { latitude: lat, longitude: lng };
+  const distance = haversine(start, end);
+  console.log(`distance between old and new coordinates is ${distance} kilometers`);
 
-const lonCell = {
-  timestamp: 0,
-  value: 0,
-};
-
-const latGps = {
-  timestamp: 0,
-  value: 0,
-};
-
-const lonGps = {
-  timestamp: 0,
-  value: 0,
-};
+  if (distance > 0.002) {
+    console.log('needs to be updated!!');
+    details[id].history.push(details[id].location);
+    details[id].location.lat = lat;
+    details[id].location.lng = lng;
+    details[id].location.timestamp = timestamp;
+  } else if (distance < 0.002) {
+    console.log('no need to be updated!!');
+  }
+}
 
 function updateMqttData() {
   const id = 3;
-  // eslint-disable-next-line no-mixed-operators,max-len
-  if (latCell.timestamp > latGps.timestamp && latCell.timestamp > latGps.timestamp + 100 || latGps.value === 0.0000) {
-    details[id].location.lat = latCell.value;
-    details[id].location.timestamp = latCell.timestamp;
-  } else {
-    details[id].location.lat = latGps.value;
-    details[id].location.timestamp = latGps.timestamp;
-  }
-
-  if (lonCell.timestamp > lonGps.timestamp || lonGps.value === 0.0000) {
-    details[id].location.lng = lonCell.value;
-    details[id].location.timestamp = lonCell.timestamp;
-  } else {
-    details[id].location.lng = lonGps.value;
-    details[id].location.timestamp = lonGps.timestamp;
+  if (latGps.value === 0.0000 || lonGps.value === 0.0000) {
+    console.log('GPS is 0.0000');
+    if (Math.abs(latCell.timestamp - lonCell.timestamp) > 1000) {
+      console.log('more than 1 second between updates - not updating');
+    } else if (Math.abs(latCell.timestamp - lonCell.timestamp) < 1000) {
+      console.log('less than 1 second between updates - passing data to next function');
+      checkForLocationUpdate(id, latCell.value, lonCell.value, Math.max(latCell.timestamp, lonCell.timestamp));
+    }
+  } else if (latGps.value !== 0.0000 || lonGps.value !== 0.0000) {
+    console.log('GPS is not 0.0000');
+    if (Math.abs(latGps.timestamp - lonGps.timestamp) > 1000) {
+      console.log('more than 1 second between updates - not updating');
+    } else if (Math.abs(latGps.timestamp - lonGps.timestamp) < 1000) {
+      console.log('less than 1 second between updates - passing data to next function');
+      checkForLocationUpdate(id, latGps.value, lonGps.value, Math.max(latGps.timestamp, lonGps.timestamp));
+    }
   }
 }
 
@@ -196,3 +218,5 @@ module.exports = {
   onMqttDataReceived,
   updateDetails,
 };
+
+// TODO location history
